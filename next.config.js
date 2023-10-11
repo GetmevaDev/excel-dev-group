cf00009c0f555c14920d49f3efc32cb71215938d
GetmevaDev <devteam@getmeva.com>
2023-10-11 14:12:04 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ["localhost", "res.cloudinary.com"],
+    unoptimized: true,
+    disableStaticImages: true,
+  },
+  swcMinify: false,
+};
+
+module.exports = nextConfig;
